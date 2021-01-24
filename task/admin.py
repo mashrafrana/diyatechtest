f482780e4330dfb231889464b4aa9006b4b31902
Ashraf Rana <programmerrana@gmail.com>
2021-01-24 23:49:23 +0500

    Update admin.py

--- task/admin.py
+++ task/admin.py
@@ -5,13 +5,15 @@ from .models import *
 # Register your models here.
 class PatientAdmin(admin.ModelAdmin):
     list_filter = ("user__name", "user__email")
-    display_list = ("user__name", "user__email")
+    search_fields =   ("user__name","user__email",)
 
 class CounsellorAdmin(admin.ModelAdmin):
     list_filter = ("user__name", "user__email")
+    search_fields =   ("user__name","user__email",)
 
 class AppointmentAdmin(admin.ModelAdmin):
     list_filter = ("patient__user", "counsellor__user","appointment_date")
+    search_fields =   ("appointment_date",)
 
 admin.site.register(Patient,PatientAdmin)
 admin.site.register(Counsellor,CounsellorAdmin)
